b8130c3178e1bcdb1101d40aff5bf522b3102d5e
Christian Diether Reyes <cbreyes4@up.edu.ph>
2018-02-01 22:55:34 +0800

    Added Reader.java

---
+++ src/Reader.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader{
+	
+	//constant variable
+	private static final int MAX_COL = 10;
+	private static final int MAX_ROW = 10;
+	private static final int INITIAL_ROW = 0;
+	public static final String KEEPER_NONSTORAGE = "k";
+	public static final String KEEPER_INSTORAGE = "K";
+
+
+	//input file to be read
+	private String filename;
+	private String[][] currentState;
+	
+	//coordinated of the keeper
+	private int xpos_keeper;
+    private int ypos_keeper;
+
+	//constructor of reader
+	public Reader(String filename){
+		this.filename = filename;
+		this.board = new Tiles[MAX_ROW][MAX_COL];
+		
+	}
+	
+
+	//reads file and returns a 2d array
+	public String[][] readFile(){
+
+		try{
+			//reader per line
+			BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+			String nextLine = reader.readLine();
+			int currentRow = INITIAL_ROW;
+			int iterator;
+	
+			while(nextLine != null){
+				String[] line = nextLine.split(" ");
+				for(iterator = 0; iterator < MAX_COL; iterator++){
+					if(line[iterator].equals(KEEPER_INSTORAGE) || line[iterator].equals(KEEPER_NONSTORAGE)){
+						this.xpos_keeper = currentRow;
+						this.ypos_keeper = iterator;
+
+						
+					}
+					this.board[currentRow][iterator] = new Tiles(currentRow, iterator, line[iterator]);
+				}
+				currentRow++;
+				nextLine = reader.readLine();
+			}
+	
+			
+			return this.board;
+		}catch(IOException ex){
+
+			System.out.println("file not found");
+
+		}	
+		
+		return this.board;
+	}
+
+	//getters
+	public int getKeeperXPos(){
+		return this.xpos_keeper;
+	}
+
+	public int getKeeperYPos(){
+		return this.ypos_keeper;
+	}
+
+}
--- src/Solver.java
+++ src/Solver.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList<E>;
 public class Solver{
-    aaa
+    //constants
+    private static final int MOVE_UP = 0;
+    private static final int MOVE_LEFT = 1;
+    private static final int MOVE_RIGHT = 2;
+    private static final int MOVE_DOWN = 3;
+
+    //arraylist of state
+    private ArrayList<State> frontier;
+
+    public Solver(){
+        this.frontier = new ArrayList<State>();
+    }
+
+    private ArrayList<int> State actions(State currentState){
+        ArrayList<int> possibleMoves = new ArrayList<int>();
+    }
+
+
 }
